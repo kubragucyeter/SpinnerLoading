0391a24dd75c618eea8743dc555201203310e22f
kübra güçyeter <kubra.gucyeter01@gmail.com>
2024-02-20 12:53:27 +0300

    son güncellemeler

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-
 const p = document.querySelector("#p");
 const button = document.querySelector("#btn");
-
+const loading = document.querySelector(".loading")
+var sql = "SELECT * FROM DCAS.PPROTECCONT";
 
 
 
 button.addEventListener("click", () => {
 
-    document.querySelector(".loading").style.display = 'block';
+    loading.style.display = 'block';
     document.querySelector("#btn").style.display = 'none';
 
-    // deger = deneme();
+    p.innerHTML = "";
 
-    deneme((deger) => {
+    deneme(
+        sql,
 
-        p.innerHTML = deger;
-        document.querySelector(".loading").style.display = 'none'
-    });
+        function (response) {
+
+            p.innerHTML = response
+            loading;
+            button;
+        },
+
+        function (failure) {
+
+            p.innerHTML = "İstek başarısızlığa düştü lütfen kontrol edin";
+            loading;
+            button;
+        },
+
+        function (error) {
+
+            p.innerHTML = "İstek başarısız";
+            loading;
+            button;
+        },
+        loading,
+        button
 
 
+    );
 });
-
-
-
-
-
-
-
 
--- loading.js
+++ loading.js
@@ -1,12 +1,7 @@
 
-
-
-function deneme() {
-
-    var sql = "SELECT * FROM DCAS.PPROTECCONT"
+function deneme(sql, callback, error, failure, loading, button) {
 
     var sqlStr = { 'sql': '' + sql + '' };
-    var returnDeger;
 
     $.ajax({
         type: "POST",
@@ -15,24 +10,30 @@ function deneme() {
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
         dataType: "json",
         async: true,
-        headers: { "Authorization": "Bearer LHcR3acemZ7d_EDfN0UdiIpnZwyNtnhsHPSH3HuanjD_1gC30UtE2tR60EaLjny1FrCszKfau8VIEkI-rmgRXetuY-GU32G88QxVNGNVMG8hnTDdPKybrBI0fehZzhoKSRp05zLG3XIqTodwmlMezqiDZ3VY9Hsb0vglO0xw-sN7IqUZ5GZ2WqV-mQoR6uRXbr8Q9UsvjROXE0nz1Iw3hHggtmxhbFI_As6kE3iFaR5JVuy-CJwq7koi7QplGtDHaa2sGfkEpBNRvB-YI_slanHkA0vUbz8HxIL5gYpMIny1cpS9hz2JouACIqXVa8wHdi_uSig07eGFiZIfr68Scg" },
+        headers: { "Authorization": "Bearer UL3VfIY6CU5dC2yJXf4dn_-G_mN6_vXzMYYQDzj2iEZb-IfdHqjZNKBKJGdS2xIkKtdWZUawkOJPRKAR4Li3e3xYkecrdHjyQOh2UI0RnEGbLZBFg6hC-YjESO2eK2e4RKR6_1U8gf6DsCimWYAmMthUtrJLmVzOgiEOOBQBxacohGTLLIJqnbsXTYdcpHGyjYTeOGfxbZSHuthPnEP6DbtocziUWTQ18xAT9ivddsKV9a6hbCImV3yYNd4lgGUb4OWEZb0jv5p-lPJVdTRcfdl29iZDh9RxOKxy9Dfyjz_igLhXEO9hY6zxr2z4n1ABR4C6mT2RKRfOHiuo8NgNVg" },
+
         success: function (response) {
-            console.log(JSON.stringify(response))
-            returnDeger = JSON.stringify(response)
+            callback(response);
+
+            loading.style.display = 'none';
+            button.style.display = 'block';
         },
-        failure: function (failure) {
-            returnDeger = failure
+
+        failure: function () {
+            failure();
+
+            loading.style.display = 'none';
+            button.style.display = 'block';
         },
-        error: function (error) {
-            returnDeger = error["statusText"]
+
+        error: function () {
+            error();
+
+            loading.style.display = 'none';
+            button.style.display = 'block';
         },
 
     });
-
-
-
-    return returnDeger;
-
 }
 
 
